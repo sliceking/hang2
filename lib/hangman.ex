ac2210e19efa0f60ace0967406078e59b7fa0bac
Stan Wielga <svwielga4@gmail.com>
2025-07-18 23:26:22 -0500

    adding hangman impl

--- lib/hangman.ex
+++ lib/hangman.ex
@@ -1,5 +1,8 @@
 defmodule Hangman do
-  @type game :: any
+
+  alias Hangman.Impl.Game
+
+  @type game :: Game.t
   @type state :: :initializing | :won | :lost | :good_guess | :bad_guess | :already_used
   @type tally :: %{
     turns_left: integer,
@@ -9,9 +12,7 @@ defmodule Hangman do
   }
 
   @spec new_game() :: game
-  def new_game do
-    
-  end
+  defdelegate new_game, to: Game
 
   @spec make_move(game, String.t) :: {game, tally}
   def make_move(game, guess) do
